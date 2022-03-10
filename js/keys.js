ffc1ce214eac7173a08794cb891ec1a6f08084b1
Irvin Ruiz <ruizirvin084@gmail.com>
2022-03-10 15:59:01 -0600

    made keys public for displaying site on github pages

---
+++ js/keys.js
@@ -0,0 +1,3 @@
+const KEY_API_MAPBOX = 'pk.eyJ1IjoibHVjaWRhd2FyZW5lc3MiLCJhIjoiY2t6ZWdlYXo5Mzd3MDJ1b2ZqbzMxMmZwNiJ9.WWbdJ3iAdqqtrNqWeKeqGg';
+
+const OPEN_WEATHER_APPID = 'fc3b40c795b41d21743a37a1bda8efab';
